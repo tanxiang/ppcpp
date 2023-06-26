b465cd2e81da359912302f36d9f8626ad78c2695
ttand <wqazsedc@gmail.com>
2023-06-27 00:09:50 +0800

    init

---
+++ main.cc
@@ -0,0 +1,14 @@
+
+#include <tensorflow/core/platform/env.h>
+#include <tensorflow/core/public/session.h>
+#include <iostream>
+
+namespace tfcc{
+
+    
+}
+
+int main(int argc,char* argv[]){
+
+    return 0;
+}
