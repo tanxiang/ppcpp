51fd56d709602349b4f523fa78bb32ef205dd44c
ttand <wqazsedc@gmail.com>
2023-07-21 01:04:16 +0800

    nn

--- main.cc
+++ main.cc
@@ -14,130 +14,108 @@
 
 namespace tfcc {
 
-auto imgDecode(std::string_view fileName, std::string_view inputName) {
-  auto root = tensorflow::Scope::NewRootScope();
-  auto file_reader = tensorflow::ops::ReadFile(root.WithOpName(inputName),
-                                               std::string{ fileName });
-  tensorflow::Output image_reader;
+namespace tf = tensorflow;
+namespace tfo = tensorflow::ops;
 
-  if (tensorflow::StringPiece(fileName).ends_with(".png")) {
-    image_reader =
-        tensorflow::ops::DecodePng(root.WithOpName("pngReader"), file_reader,
-                                   tensorflow::ops::DecodePng::Channels(1));
-  } else if (tensorflow::StringPiece(fileName).ends_with(".gif")) {
-    image_reader =
-        tensorflow::ops::DecodeGif(root.WithOpName("gifReader"), file_reader);
-  } else {
-    image_reader =
-        tensorflow::ops::DecodeJpeg(root.WithOpName("jpgReader"), file_reader);
-  }
-  return image_reader;
+auto imgDecode(std::string_view fileName, std::string_view inputName)
+{
+    auto root = tf::Scope::NewRootScope();
+    auto file_reader = tfo::ReadFile(root.WithOpName(inputName), std::string { fileName });
+    tf::Output image_reader;
+
+    if (tf::StringPiece(fileName).ends_with(".png")) {
+        image_reader = tfo::DecodePng(root.WithOpName("pngReader"), file_reader, tfo::DecodePng::Channels(1));
+    } else if (tf::StringPiece(fileName).ends_with(".gif")) {
+        image_reader = tfo::DecodeGif(root.WithOpName("gifReader"), file_reader);
+    } else {
+        image_reader = tfo::DecodeJpeg(root.WithOpName("jpgReader"), file_reader);
+    }
+    return image_reader;
 }
 
-auto active(tensorflow::Scope &scope, tensorflow::Input inputs) {
-  return tensorflow::ops::Relu6(scope.WithOpName("relu6"), inputs);
+auto active(tf::Scope& scope, tf::Input inputs)
+{
+    return tfo::Relu6(scope.WithOpName("relu6"), inputs);
 }
 
-auto dense(tensorflow::Scope &scope, tensorflow::Input inputs, int in_units,
-           int out_units) {
-    auto inputShape = tensorflow::ops::Shape(scope.WithOpName("shape"),inputs);
-    auto weightShape = tensorflow::ops::Concat(scope.WithOpName("apppend"),{inputShape.output,tensorflow::ops::Const(scope, { out_units })},0);
-  auto weightsInitial = tensorflow::ops::RandomNormal(
-      scope, inputShape,inputShape.output.type());
+auto Dense(tf::Scope& scope, tf::Output inputs, int in_units, int out_units)
+{
+    auto inputShape = tfo::Shape(scope.WithOpName("shape"), inputs);
+    auto weightShape = tfo::Concat(scope.WithOpName("apppend"), { inputShape.output, tfo::Const(scope, { out_units }) }, 0);
+    auto weightsInitial = tfo::RandomNormal(scope, inputShape, inputShape.output.type());
 
-  auto weight = tensorflow::ops::Variable(
-      scope.WithOpName("weight"), { in_units, out_units }, inputs.data_type());
+    auto weight = tfo::Variable(scope.WithOpName("weight"), { in_units, out_units }, inputs.type());
 
-  tensorflow::ops::Assign(scope, weight, weightsInitial);
+    tfo::Assign(scope, weight, weightsInitial);
 
-  auto biases = tensorflow::ops::Variable(scope.WithOpName("biases"),
-                                          { out_units }, inputs.data_type());
-  tensorflow::ops::Assign(scope, biases,
-                          tensorflow::Input::Initializer(0.f, { out_units }));
-  return tensorflow::ops::Add(
-      scope.WithOpName("AddBiases"),
-      tensorflow::ops::MatMul(scope.WithOpName("MatMulWeight"), inputs, weight),
-      biases);
+    auto biases = tfo::Variable(scope.WithOpName("biases"), { out_units }, inputs.type());
+    tfo::Assign(scope, biases, tf::Input::Initializer(0.f, { out_units }));
+
+    return tfo::Add(scope.WithOpName("AddBiases"), tfo::MatMul(scope.WithOpName("MatMulWeight"), inputs, weight), biases);
 }
 
-auto convPack(tensorflow::Scope scope, tensorflow::Output inputs, int filters,
-              std::array<int, 2> kernelShape = { 3, 3 },
-              std::array<int, 2> strides = { 1, 1 }) {
-  auto convPad =
-      tensorflow::ops::Pad(scope.WithOpName("pad0"), inputs,
-                           { { 0, 0 }, { 1, 1 }, { 1, 1 }, { 0, 0 } });
+auto Conv(tf::Scope scope, tf::Output inputs, int filters, int channel = 1, std::array<int, 2> kernelShape = { 3, 3 }, std::array<int, 2> strides = { 1, 1 })
+{
+    auto convPad = tfo::Pad(scope.WithOpName("pad0"), inputs, { { 0, 0 }, { 1, 1 }, { 1, 1 }, { 0, 0 } });
 
-  auto inputShape = tensorflow::ops::Shape(scope.WithOpName("Shape"),inputs);
-  //  std::cout << inputShape.node()->DebugString() << std::endl;
+    auto inputShape = tfo::Shape(scope.WithOpName("Shape"), inputs);
+    //  std::cout << inputShape.node()->DebugString() << std::endl;
 
-  auto weightsInitial = tensorflow::ops::RandomNormal(
-      scope, tensorflow::ops::Const(scope, { kernelShape[0], kernelShape[1] }),
-      inputs.type());
+    auto weightsInitial = tfo::RandomNormal(scope, tfo::Const(scope, { kernelShape[0], kernelShape[1] }), inputs.type());
 
-  auto weight = tensorflow::ops::Variable(
-      scope, { filters, kernelShape[0], kernelShape[1], 1 },
-      inputs.type());
-  tensorflow::ops::Assign(scope, weight, weightsInitial);
+    auto weight = tfo::Variable(scope, { filters, kernelShape[0], kernelShape[1], channel }, inputs.type());
+    tfo::Assign(scope, weight, weightsInitial);
 
-  auto biases =
-      tensorflow::ops::Variable(scope, { filters }, inputs.type());
-  tensorflow::ops::Assign(scope, biases,
-                          tensorflow::Input::Initializer(0.f, { filters }));
+    auto biases = tfo::Variable(scope, { filters }, inputs.type());
+    tfo::Assign(scope, biases, tf::Input::Initializer(0.f, { filters }));
 
-  auto convOutput = tensorflow::ops::Conv2D(
-      scope.WithOpName("conv"), convPad, weight,
-      { 1, strides[0], strides[1], 1 }, std::string{ "SAME" });
+    auto convOutput = tfo::Conv2D(scope.WithOpName("conv"), convPad, weight, { filters, strides[0], strides[1], channel }, std::string { "SAME" });
 
-  return active(scope, tensorflow::ops::BiasAdd(scope.WithOpName("bias"),convOutput,biases));
+    return active(scope, tfo::BiasAdd(scope.WithOpName("bias"), convOutput, biases));
 }
 
-auto Dropout(tensorflow::Scope scope, tensorflow::Input inputs) {
+auto Dropout(tf::Scope scope, tf::Input inputs) { }
 
+auto Flatten(tf::Scope scope, tf::Input inputs)
+{
+    return tfo::Reshape(scope.WithOpName("reshape"), inputs, { -1 });
 }
 
-auto Flatten(tensorflow::Scope scope, tensorflow::Input inputs) {
- return tensorflow::ops::Reshape(scope.WithOpName("reshape"), inputs, { -1 });
+auto Gap(tf::Scope scope, tf::Input inputs)
+{
+    auto inputShape = tfo::Shape(scope.WithOpName("shape"), inputs);
+    // return tfo::AvgPool(scope, inputs, {-1});
 }
 
-auto Gap(tensorflow::Scope scope, tensorflow::Input inputs) {
-   //return tensorflow::ops::AvgPool(scope, inputs, {-1});
+auto buildInputBlocks(tf::Scope scope,tf::Output input)
+{
+    return tfcc::Conv(scope.NewSubScope("conv0"), input, 1);
 }
 
-auto buildInputBlocks(tensorflow::Scope scope,
-                      tensorflow::ops::Placeholder &input) {
+auto buildPPC(tf::Scope rootScope, tfo::Placeholder& input,
+    tfo::Placeholder& output)
+{
+    tf::Output x = buildInputBlocks(rootScope.NewSubScope("head"), input);
+    x = buildInputBlocks(rootScope.NewSubScope("head1"), x);
+    x = buildInputBlocks(rootScope.NewSubScope("head2"), x);
+    x = Flatten(rootScope.NewSubScope("head2"),x);
 
-  return tfcc::convPack(scope.NewSubScope("conv0"), input, 1);
+    return x;
 }
 
-auto buildPPC(tensorflow::Scope rootScope, tensorflow::ops::Placeholder &input,
-              tensorflow::ops::Placeholder &output) {
-  auto output0 = buildInputBlocks(rootScope.NewSubScope("head"), input);
-  return output0;
-}
+} // namespace tfcc
 
-}  // namespace tfcc
+int main(int argc, char* argv[])
+{
+    auto rootScope = tensorflow::Scope::NewRootScope();
+    auto input0 = tensorflow::ops::Placeholder { rootScope, tensorflow::DT_FLOAT, tensorflow::ops::Placeholder::Shape({ 1, 512, 512, 1 }) };
+    auto outputBox = tensorflow::ops::Placeholder { rootScope, tensorflow::DT_FLOAT, tensorflow::ops::Placeholder::Shape({ 1, 4, 3200, 1 }) };
 
-int main(int argc, char *argv[]) {
-  auto rootScope = tensorflow::Scope::NewRootScope();
+    auto output = tfcc::buildPPC(rootScope.NewSubScope("ppcpp"), input0, outputBox);
+    auto graph = rootScope.graph_as_shared_ptr();
+    auto cSession = tensorflow::ClientSession { rootScope };
 
-  auto input0 = tensorflow::ops::Placeholder{
-    rootScope, tensorflow::DT_FLOAT,
-    tensorflow::ops::Placeholder::Shape({ 1, 512, 512, 1 })
-  };
+    // tensorflow::ops::ApplyAdam adam{};
 
-  auto outputBox = tensorflow::ops::Placeholder{
-    rootScope, tensorflow::DT_FLOAT,
-    tensorflow::ops::Placeholder::Shape({ 1, 4, 3200, 1 })
-  };
-
-  auto output =
-      tfcc::buildPPC(rootScope.NewSubScope("ppcpp"), input0, outputBox);
-
-  auto graph = rootScope.graph_as_shared_ptr();
-
-  auto cSession = tensorflow::ClientSession{ rootScope };
-
-  // tensorflow::ops::ApplyAdam adam{};
-
-  return 0;
+    return 0;
 }
